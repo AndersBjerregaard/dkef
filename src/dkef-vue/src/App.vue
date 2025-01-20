4b161d5123440cc38ac88a02fa3dd630ffc02d4a
Anders Nielsen <an@betterboard.dk>
2025-01-20 07:41:29 +0100

    boilerplate html

--- src/dkef-vue/src/App.vue
+++ src/dkef-vue/src/App.vue
@@ -9,6 +9,12 @@ import { RouterView } from 'vue-router'
       <!-- HelloWorld msg="You did it!" /> -->
 
       <nav>
+        <img width="125" height="125" alt="DKEF logo">
+        <button>Medlemsfordele</button>
+        <button>Arrangementer og nyheder</button>
+        <button>Om foreningen</button>
+        <button>Kontakt os</button>
+        <button>Log på</button>
         <!-- <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink> -->
       </nav>
--- src/dkef-vue/src/components/TheWelcome.vue
+++ src/dkef-vue/src/components/TheWelcome.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Elektroteknisk Forening</h1>
+  <h1>Dit netværk i elbranchen</h1>
+
+  <h2>Udvid din faglige horisont, blev en del af fællesskabet og modtag sparring fra erfarne hænder.</h2>
+
+  <button>Medlemsfordele</button>
+
+  <button>Eksisterende Medlemmer</button>
+
+  <p>
+    4.8/5 stjerner af vores 100+ anmeldelser.
+  </p>
+
+  <img width="500" height="500" alt="An electrical mast">
 </template>
