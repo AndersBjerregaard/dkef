fe088e2945438dd3af01161246176ccfb05a4ed5
Anders Stræde Bjerregaard Nielsen <don-sbn@hotmail.com>
2025-02-04 09:35:57 +0100

    added burger buttons

--- src/dkef-vue/src/App.vue
+++ src/dkef-vue/src/App.vue
@@ -17,7 +17,7 @@ const isOpen = ref(false);
         </RouterLink>
         <!-- Inline buttons (visible on larger screens ) -->
         <div class="hidden md:flex md:w-full">
-          <div class="flex p-4 justify-end items-center">
+          <div class="flex p-4 w-full justify-end items-center">
             <div class="p-3">
               <button class="rounded bg-gray-600 h-12 w-36 p-2 cursor-pointer hover:bg-gray-800">Medlemsfordele</button>
             </div>
@@ -43,20 +43,50 @@ const isOpen = ref(false);
           <div v-if="isOpen" class="flex items-center justify-center">
             <p class="cursor-pointer rounded bg-gray-600 h-12 w-36 p-3 hover:bg-gray-800 text-center">Burger button</p>
           </div> -->
-          <Menu>
-            <MenuButton>More</MenuButton>
-            <MenuItems>
-              <MenuItem>
-                <span>Burger item 1</span>
-              </MenuItem>
-              <MenuItem>
-                <span>Burger item 2</span>
-              </MenuItem>
-              <MenuItem>
-                <span>Burger item 3</span>
-              </MenuItem>
-            </MenuItems>
-          </Menu>
+          <div class="fixed text-right">
+            <Menu>
+              <MenuButton
+                class="text-2xl rounded bg-gray-600 h-12 w-20 p-2 cursor-pointer hover:bg-gray-800">
+                  ≡</MenuButton>
+              <transition
+                enter-active-class="transition duration-100 ease-out"
+                enter-from-class="transform scale-95 opacity-0"
+                enter-to-class="transform scale-100 opacity-100"
+                leave-active-class="transition duration-75 ease-in"
+                leave-from-class="transform scale-100 opacity-100"
+                leave-to-class="transform scale-95 opacity-0">
+                <MenuItems class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-600 rounded-md bg-gray-700 shadow-lg ring-1 ring-black/5 focus:outline-none">
+                  <div class="px-1 py-1">
+                    <MenuItem v-slot="{ active }">
+                      <button :class="[active ? 'bg-gray-800' : 'bg-gray-600', 'group flex w-full items-center rounded-md px-2 py-2 text-sm cursor-pointer']">
+                        Medlemsfordele
+                      </button>
+                    </MenuItem>
+                    <MenuItem v-slot="{ active }">
+                      <button :class="[active ? 'bg-gray-800' : 'bg-gray-600', 'group flex w-full items-center rounded-md px-2 py-2 text-sm cursor-pointer']">
+                        Arrangementer og nyheder
+                      </button>
+                    </MenuItem>
+                    <MenuItem v-slot="{ active }">
+                      <button :class="[active ? 'bg-gray-800' : 'bg-gray-600', 'group flex w-full items-center rounded-md px-2 py-2 text-sm cursor-pointer']">
+                        Om foreningen
+                      </button>
+                    </MenuItem>
+                    <MenuItem v-slot="{ active }">
+                      <button :class="[active ? 'bg-gray-800' : 'bg-gray-600', 'group flex w-full items-center rounded-md px-2 py-2 text-sm cursor-pointer']">
+                        Kontakt os
+                      </button>
+                    </MenuItem>
+                    <MenuItem v-slot="{ active }">
+                      <button :class="[active ? 'bg-gray-800' : 'bg-gray-600', 'group flex w-full items-center rounded-md px-2 py-2 text-sm cursor-pointer']">
+                        Log på
+                      </button>
+                    </MenuItem>
+                  </div>
+                </MenuItems>
+              </transition>
+            </Menu>
+          </div>
         </div>
       </nav>
     </div>
