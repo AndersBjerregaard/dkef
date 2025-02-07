5ee20db6435595244685c8abfea8ad365ed66a6b
Anders Stræde Bjerregaard Nielsen <don-sbn@hotmail.com>
2025-02-07 17:37:16 +0100

    almost done responsiveness on phones

--- src/dkef-vue/src/components/MemberAdvantages.vue
+++ src/dkef-vue/src/components/MemberAdvantages.vue
@@ -4,14 +4,14 @@
   <!-- First section -->
   <div class="flex flex-col md:flex-row justify-center items-center gap-x-8 px-4 py-4 sm:py-16 mx-auto">
     <div class="gap-4 w-screen md:w-96 px-2 pb-4 md:py-16 mx-auto">
-      <h1 class="text-lg md:text-4xl pb-12">Bliv medlem af Elektroteknisk forening i dag</h1>
+      <h1 class="text-2xl md:text-4xl pb-12">Bliv medlem af Elektroteknisk forening i dag</h1>
       <p class="pb-12">
         Udvid dit netværk, opnå nye kompetencer, og accelerér din udvikling som studerende i elbranchen med overskuelige priser
       </p>
       <button class="flex justify-center rounded bg-gray-600 h-10 sm:h-12 py-2 w-24 sm:w-32 cursor-pointer hover:bg-gray-800 sm:text-lg">Bliv medlem</button>
     </div>
 
-    <div class="gap-4 w-96 mx-auto">
+    <div class="gap-4 w-screen md:w-96 px-2 pb-4 md:py-16 mx-auto">
       <div class="border bg-gray-700 rounded-2xl p-2">
         <div class="flex justify-center">
           <h2 class="text-xl py-4">Medlemskab</h2>
@@ -38,29 +38,29 @@
   </div>
 
   <!-- Second section -->
-  <div class="flex justify-center gap-x-8 px-4 py-16">
-    <div class="w-96 gap-4 items-start">
-      <h1 class="text-4xl pb-12">Få adgang til netværksdatabase</h1>
+  <div class="flex flex-col md:flex-row justify-center items-center gap-x-8 px-4 py-4 sm:py-16 mx-auto">
+    <div class="gap-4 w-screen md:w-96 px-2 pb-4 md:py-16 mx-auto">
+      <h1 class="text-2xl sm:text-4xl pb-12">Få adgang til netværksdatabase</h1>
       <p class="pb-12">
         Søger du specifikke kompetencer og indsigter i elbranchen? Som medlem i Elektroteknisk forening har du agang til en komplet oversigt over andre medlemmer, som du kan søge sparring og ekspertise hos.
       </p>
     </div>
 
-    <div class="w-96 gap-4 items-start">
+    <div class="hidden sm:flex w-96 gap-4 items-start">
       <img src="@/assets/advantages1.jpeg" alt="member advantages">
     </div>
   </div>
 
   <!-- Third section -->
-  <div class="flex justify-center gap-x-8 px-4 py-16">
-    <div class="w-96 gap-4 items-start">
-      <h1 class="text-4xl pb-12">Deltag i arrangementer og studieture</h1>
+  <div class="flex flex-col md:flex-row justify-center items-center gap-x-8 px-4 py-4 sm:py-16 mx-auto">
+    <div class="gap-4 w-screen md:w-96 px-2 pb-4 md:py-16 mx-auto">
+      <h1 class="text-2xl sm:text-4xl pb-12">Deltag i arrangementer og studieture</h1>
       <p class="pb-12">
         Vil du styrke din faglighed og få et bredt kendskab til elbranchen? I Elektroteknisk forening afholder vi studieture og besøger virksomheder, så du kan udvikle dine kompetencer og opnå ny viden.
       </p>
     </div>
 
-    <div class="w-96 gap-4 items-start">
+    <div class="hidden sm:flex w-96 gap-4 items-start">
       <img src="@/assets/advantages2.jpeg" alt="member advantages">
     </div>
   </div>
