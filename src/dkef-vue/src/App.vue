8b8582d0a458ef309a4765c76d78619306ed48e0
Anders Nielsen <don-sbn@hotmail.com>
2025-02-16 18:07:32 +0100

    added headlessui boilerplate to login modal

--- src/dkef-vue/src/App.vue
+++ src/dkef-vue/src/App.vue
@@ -1,6 +1,21 @@
 <script setup lang="ts">
 import { RouterView, RouterLink } from 'vue-router'
-import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
+import {
+  Menu, MenuButton, MenuItems, MenuItem,
+  TransitionRoot, TransitionChild,
+  Dialog, DialogPanel, DialogTitle
+} from '@headlessui/vue'
+import { ref } from 'vue'
+
+const isOpen = ref(false)
+
+function closeModal() {
+  isOpen.value = false
+}
+
+function openModal() {
+  isOpen.value = true
+}
 </script>
 
 <template>
@@ -40,10 +55,49 @@ import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
               </RouterLink>
             </div>
             <div class="p-3">
-              <button class="rounded bg-gray-600 h-12 w-20 p-2 cursor-pointer hover:bg-gray-800">Log på</button>
+              <button class="rounded bg-gray-600 h-12 w-20 p-2 cursor-pointer hover:bg-gray-800" @click="openModal">
+                Log på
+              </button>
             </div>
           </div>
         </div>
+        <TransitionRoot appear :show="isOpen" as="template">
+          <Dialog as="div" @close="closeModal" class="relative z-10">
+            <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
+              leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
+              <div class="fixed inset-0 bg-black/25"></div>
+            </TransitionChild>
+
+            <div class="fixed inset-0 overflow-y-auto">
+              <div class="flex min-h-full items-center justify-center p-4 text-center">
+                <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
+                  enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
+                  leave-to="opacity-0 scale-95">
+                  <DialogPanel
+                    class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                    <DialogTitle as="h3" class="text-lg font-medium leading-6 text-gray-900">
+                      Payment successful
+                    </DialogTitle>
+                    <div class="mt-2">
+                      <p class="text-sm text-gray-500">
+                        Your payment has been successfully submitted. We’ve sent you
+                        an email with all of the details of your order.
+                      </p>
+                    </div>
+
+                    <div class="mt-4">
+                      <button type="button"
+                        class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                        @click="closeModal">
+                        Got it, thanks!
+                      </button>
+                    </div>
+                  </DialogPanel>
+                </TransitionChild>
+              </div>
+            </div>
+          </Dialog>
+        </TransitionRoot>
 
         <!-- Burger menu (visible on small screens) -->
         <div class="md:hidden flex w-full sm:p-4 justify-end items-center">
