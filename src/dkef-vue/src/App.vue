6ef27b8dcd3f186f00a22215fddef51430773992
Anders Nielsen <don-sbn@hotmail.com>
2025-02-27 22:10:38 +0100

    login

--- src/dkef-vue/src/App.vue
+++ src/dkef-vue/src/App.vue
@@ -76,20 +76,19 @@ function openModal() {
                   <DialogPanel
                     class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                     <DialogTitle as="h3" class="text-lg font-medium leading-6 text-gray-900">
-                      Payment successful
+                      Login
                     </DialogTitle>
                     <div class="mt-2">
                       <p class="text-sm text-gray-500">
-                        Your payment has been successfully submitted. We’ve sent you
-                        an email with all of the details of your order.
+                        Du er nu logget på.
                       </p>
                     </div>
 
                     <div class="mt-4">
                       <button type="button"
-                        class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                        class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 cursor-pointer"
                         @click="closeModal">
-                        Got it, thanks!
+                        Fortsæt til DKEF
                       </button>
                     </div>
                   </DialogPanel>
