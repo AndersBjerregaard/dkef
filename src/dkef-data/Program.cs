0ef3dce3b581d7530ca42e3035132bf35339dfa1
Anders Nielsen <an@betterboard.dk>
2025-03-03 07:59:24 +0100

    domain model and database docker compose

--- src/dkef-data/Program.cs
+++ src/dkef-data/Program.cs
@@ -1,11 +1,18 @@
-﻿string filePath = "data/denero_contacts.csv";
+﻿using Domain;
+
+string filePath = "data/denero_contacts.csv";
 
 using var reader = new StreamReader(filePath);
 
+// Skip first line since they're just headers
+reader.ReadLine();
+
 string? line;
+string[] values;
 
 while ((line = reader.ReadLine()) != null) {
 	Console.WriteLine(line);
+	values = line.Split(';', StringSplitOptions.None);
 }
 
 Console.WriteLine("DONE");
