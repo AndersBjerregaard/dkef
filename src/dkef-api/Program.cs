beef1167404f8a3bc6fba1699f49f56186e2b965
Anders Nielsen <don-sbn@hotmail.com>
2025-03-16 15:41:59 +0100

    ef core

--- src/dkef-api/Program.cs
+++ src/dkef-api/Program.cs
@@ -1,3 +1,5 @@
+using Dkef.Data;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var dbConString = builder.Configuration.GetConnectionString("PostgresDb");
+
+builder.Services.AddDbContext<ContactContext>(options => options.UseNpgsql(dbConString));
 
 var app = builder.Build();
 
