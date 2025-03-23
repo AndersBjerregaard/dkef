f4b770590aabd8b8f3615affab8d0ab376f6f3b6
Anders Nielsen <don-sbn@hotmail.com>
2025-03-23 19:27:14 +0100

    wip list rendering

--- src/dkef-vue/src/components/MembersComponent.vue
+++ src/dkef-vue/src/components/MembersComponent.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const items = ref([{ name: 'Nam' }, { name: 'Num' }])
+</script>
 
 <template>
   <div class="flex justify-center items-center">
@@ -6,7 +10,9 @@
       <h1 class="text-4xl py-8">Members List</h1>
     </div>
     <ul>
+      <li v-for="(item, index) in items" v-bind:key="index">
 
+      </li>
     </ul>
   </div>
 </template>
