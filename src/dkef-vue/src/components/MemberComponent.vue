5ed2694d81e4f8edc55d7ad96a3af512409db5f7
Anders Nielsen <an@betterboard.dk>
2025-06-25 07:50:27 +0200

    click events and stuff

--- src/dkef-vue/src/components/MemberComponent.vue
+++ src/dkef-vue/src/components/MemberComponent.vue
@@ -3,25 +3,29 @@ import { type Contact } from '@/types/members'
 
 const props = defineProps<{ contact: Contact, index: number }>();
 
+const fields = [
+  props.contact?.firstName,
+  props.contact?.email,
+  props.contact?.privatePhone,
+  props.contact?.primarySection,
+  props.contact?.privateAddress
+];
+
+const handleFieldClick = (fieldValue: string | undefined, fieldIndex: number) => {
+  console.info('Field clicked: ', fieldValue);
+  console.info('Index clicked: ', fieldIndex);
+};
+
 </script>
 
 <template>
   <div class="border-x-2 border-gray-900 w-full justify-between flex"
     :class="{ 'bg-black': index % 2 == 0, 'bg-gray-800': index % 2 == 1 }">
-    <div class="border-2 border-gray-900 h-10 py-1 px-1 flex-1 min-w-0 flex justify-center">
-      <span class="truncate">{{ props.contact?.firstName }}</span>
-    </div>
-    <div class="border-2 border-gray-900 h-10 py-1 px-1 flex-1 min-w-0 flex justify-center">
-      <span class="truncate">{{ props.contact?.email }}</span>
-    </div>
-    <div class="border-2 border-gray-900 h-10 py-1 px-1 flex-1 min-w-0 flex justify-center">
-      <span class="truncate">{{ props.contact?.privatePhone }}</span>
-    </div>
-    <div class="border-2 border-gray-900 h-10 py-1 px-1 flex-1 min-w-0 flex justify-center">
-      <span class="truncate">{{ props.contact?.primarySection }}</span>
-    </div>
-    <div class="border-2 border-gray-900 h-10 py-1 px-1 flex-1 min-w-0 flex justify-center">
-      <span class="truncate">{{ props.contact?.privateAddress }}</span>
+    <div v-for="(field, i) in fields"
+      :key="i"
+      class="border-2 border-gray-900 h-10 py-1 px-1 flex-1 min-w-0 flex justify-center"
+      @click="handleFieldClick(field, i)">
+      <span class="truncate">{{ field }}</span>
     </div>
   </div>
 </template>
--- src/dkef-vue/src/components/MemberHeaderComponent.vue
+++ src/dkef-vue/src/components/MemberHeaderComponent.vue
@@ -1,15 +1,38 @@
 <script setup lang="ts">
+import { ref, computed } from 'vue';
 import { Sort } from '@/types/members';
 
 const props = defineProps<{ header: string, sort: Sort }>()
+const emit = defineEmits(['update:sort']);
 
-// TODO: Render header different depending on sort, and create sort click event to emit to parent component
+const currentSortDirection = ref(props.sort);
 
+const sortArrow = computed(() => {
+  if (props.sort === Sort.None) {
+    return '';
+  }
+  return currentSortDirection.value === Sort.Asc ? '&#9650;' : '&#9660;'; // Up arrow or down arrow
+});
+
+const handleClick = () => {
+  let newDirection = Sort.Asc;
+  if (props.sort === Sort.None) {
+    // Toggle direction if already sorting by this header
+    newDirection = currentSortDirection.value === Sort.Asc ? Sort.Desc : Sort.Asc;
+  }
+  currentSortDirection.value = newDirection; // Update internal state
+
+  // Emit event to parent with new information
+  emit('update:sort', { key: props.header, sort: newDirection });
+}
 </script>
 
 <template>
-  <div class="border-x-2 border-gray-800 w-full py-1">
-    <h2 class="text-lg overflow-ellipsis">{{ props.header }}</h2>
+  <div class="border-x-2 border-gray-800 w-full py-1 cursor-pointer" @click="handleClick">
+    <h2 class="text-lg overflow-ellipsis flex items-center justify-between">
+      <span>{{ props.header }}</span>
+      <span v-html="sortArrow" class="ml-2"></span>
+    </h2>
   </div>
 </template>
 
