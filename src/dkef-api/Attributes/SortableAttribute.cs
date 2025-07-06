0b67cd28041ff2f427d07acea8da897a61474b18
Anders Nielsen <don-sbn@hotmail.com>
2025-07-06 10:55:20 +0200

    Description on 'SortableAttribute'

--- src/dkef-api/Attributes/SortableAttribute.cs
+++ src/dkef-api/Attributes/SortableAttribute.cs
@@ -1,7 +1,7 @@
 namespace Dkef.Attributes;
 
 /// <summary>
-/// Represents a property that's able to be queryed to be sorted by at repository level
+/// Indicates a property that's able to be queryed to be sorted by at repository level
 /// </summary>
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
 public class SortableAttribute : Attribute { }
