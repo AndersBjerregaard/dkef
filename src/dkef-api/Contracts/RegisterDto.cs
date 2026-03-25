73ffe72b78965b484429f5bb7702e78fb3ceb5f8
AndersBjerregaard <don-sbn@hotmail.com>
2026-03-25 13:22:43 +0100

    Made name required for register

--- src/dkef-api/Contracts/RegisterDto.cs
+++ src/dkef-api/Contracts/RegisterDto.cs
@@ -9,12 +9,13 @@ public record RegisterDto
     public required string Email { get; init; }
 
     [Required(ErrorMessage = "Password is required")]
-    [MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
     public required string Password { get; init; }
 
     [Required(ErrorMessage = "Password confirmation is required")]
     [Compare("Password", ErrorMessage = "Password and confirmation password do not match")]
     public required string ConfirmPassword { get; init; }
 
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Name is required")]
     public string Name { get; init; } = string.Empty;
 }
