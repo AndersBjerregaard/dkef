7691bec355f1d0de6761ed20976cdd3b1f573065
Anders Nielsen <don-sbn@hotmail.com>
2026-09-12 19:25:45 +0200

    Better ux when navigating events and news

--- src/dkef-vue/src/router/index.ts
+++ src/dkef-vue/src/router/index.ts
@@ -4,6 +4,17 @@ import { useAuthStore } from '@/stores/authStore'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+
+    if (to.hash) {
+      return { el: to.hash }
+    }
+
+    return { top: 0 }
+  },
   routes: [
     {
       path: '/',
